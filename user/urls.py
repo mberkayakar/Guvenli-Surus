42315242365348b3a02769f05038f28f764a28a2
Berkay <m.berkay.akar@gmail.com>
2021-01-14 19:45:43 +0300

    users.urls düzeltildi

--- user/urls.py
+++ user/urls.py
@@ -15,18 +15,7 @@ urlpatterns = [
     path('sil/',sil),
     path('kisisel/',kisisel),
 
-#   path('account/', include('django.contrib.auth.urls')),
-#   path('password-reset/', auth_views.PasswordResetView.as_view( template_name='commons/password-reset/password_reset.html', subject_template_name='commons/password-reset/password_reset_subject.txt', email_template_name='commons/password-reset/password_reset_email.html', ), name='password_reset'),
-#   path('password-reset/done/', auth_views.PasswordResetDoneView.as_view( template_name='commons/password-reset/password_reset_done.html' ), name='password_reset_done'),
-#   path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view( template_name='commons/password-reset/password_reset_confirm.html'  ), name='password_reset_confirm'),
-#   path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view( template_name='commons/password-reset/password_reset_complete.html' ), name='password_reset_complete'),
-
-    path('reset_password/',auth_views.PasswordResetView.as_view()),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view()),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view()),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view()),
-    
-
+ 
 
 
  
